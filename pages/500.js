fd761d240e5c752775284f6c1936772289c981ce
Lucas Koval <lucaskovaldev@gmail.com>
2021-11-16 11:59:00 -0300

    Template final changes

--- pages/500.js
+++ pages/500.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import Image from 'next/image'
-import { MainSection, PageContainer, ErrorContainer, Title } from '@/styles/globalStyles'
+import { MainSection, PageContainer, ErrorPageContainer, Title } from '@/styles/globalStyles'
 
 export default function Custom500() {
   return (
     <MainSection className="MainSection">
       <PageContainer className="PageContainer">
-        <ErrorContainer>
+        <ErrorPageContainer>
           <Title className="notFound">
             Oops!.. <br /> Server-side error occurred (500)
           </Title>
           <Image src="/img/not-found.gif" alt="Not found" width="480" height="270" />
-        </ErrorContainer>
+        </ErrorPageContainer>
       </PageContainer>
     </MainSection>
   )
